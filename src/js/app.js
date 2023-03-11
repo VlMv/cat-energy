e5b1a76ff3148009faf267e992b9fd8dc6988cd7
Vasil <vasilmuktasipov@gmail.com>
2023-03-11 15:15:53 +0400

    inputs got stylized, focus statement got stylized

--- src/js/app.js
+++ src/js/app.js
@@ -1,18 +1,51 @@
 const burger = document.querySelector(".burger");
 const burgerBars = document.querySelector(".burger-bar");
 const nav = document.querySelector(".main-nav");
+const navLinks = document.querySelectorAll(".main-nav__item a");
+
+
+function setTabIndex() {
+   const burgerStyles = window.getComputedStyle(burger);
+
+   if (burgerStyles.getPropertyValue("display") !== 'none' && !burger.style.getPropertyValue("--_scale")) {
+      navLinks.forEach(navLink => {
+         navLink.setAttribute("tabindex", "-1")
+      });
+
+   } else {
+      navLinks.forEach(navLink => {
+         navLink.removeAttribute("tabindex")
+      });
+   }
+};
+window.addEventListener('load', () => {
+   setTabIndex();
+});
+window.addEventListener('resize', () => {
+   setTabIndex();
+});
+
 
 burger.addEventListener('click', function () {
    if (!burger.style.getPropertyValue("--_scale") && !burgerBars.style.getPropertyValue("--_shrink")) {
       burger.style.setProperty("--_scale", "1");
       burgerBars.style.setProperty("--_shrink", "0");
       burger.setAttribute("aria-expanded", "true");
+
+      navLinks.forEach(navLink => {
+         navLink.removeAttribute("tabindex")
+      });
       nav.style.setProperty("--_expand", "1");
       nav.setAttribute("aria-hidden", "false");
+
    } else {
       burger.removeAttribute('style');
       burgerBars.removeAttribute('style');
       burger.setAttribute("aria-expanded", "false");
+
+      navLinks.forEach(navLink => {
+         navLink.setAttribute("tabindex", "-1")
+      });
       nav.removeAttribute('style');
       nav.setAttribute("aria-hidden", "true");
    }
@@ -28,6 +61,7 @@ catShapeToogleBtn.addEventListener('click', function () {
       catSkinny.style.setProperty("--_opacity", "1");
       catFat.style.setProperty("--_opacity", "0");
       catShapeToogleBtn.style.setProperty("--_active", "1");
+
    } else {
       catSkinny.removeAttribute('style');
       catFat.removeAttribute('style');
@@ -41,6 +75,7 @@ const catShapeRangeSlider = document.querySelector(".cat-img-controls__range-sli
 catShapeRangeSlider.oninput = () => {
    let rangeSliderValue = catShapeRangeSlider.value;
    let maskWidthCatFat = 100 - rangeSliderValue;
+
    catFat.style.setProperty("--_mask-width", ' ' + maskWidthCatFat + '%');
    catSkinny.style.setProperty("--_mask-width", ' ' + rangeSliderValue + '%');
 };
